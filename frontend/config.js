179489db201abe2c0573987cb38bfe0909da2f72
Shakhzod <shakhsage@gmail.com>
2026-07-15 00:31:10 +0500

    render link backend

--- frontend/config.js
+++ frontend/config.js
@@ -4,5 +4,5 @@
 // Local sinovda:      "http://127.0.0.1:8000"
 // Render'ga chiqqach:  "https://sizning-backend.onrender.com"
 window.APP_CONFIG = {
-  API_BASE: "http://127.0.0.1:8000",
+  API_BASE: "https://erp-mvp-mqlv.onrender.com",
 };
